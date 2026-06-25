e65fa3f3d8e6335f9f83d0519d83a431f097df2d
Ivy Chen <ivy0925chen@gmail.com>
2026-06-25 18:33:19 +0800

    Initial Commit

---
+++ KindleQuotes/KindleQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindleQuotesApp.swift
+//  KindleQuotes
+//
+//  Created by Ivy Chen on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct KindleQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
